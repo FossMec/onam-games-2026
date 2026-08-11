d4196a574941cb409390c9cbe04e7e195581b423
dijith-481 <dijithdinesh@protonmail.com>
2026-08-11 23:28:21 +0530

    Initial commit from Vite+

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite-plus";
+import { nitro } from "nitro/vite";
+
+import { solidStart } from "@solidjs/start/config";
+import { lazyPlugins } from "vite-plus";
+
+export default defineConfig({
+  staged: {
+    "*": "vp check --fix",
+  },
+  fmt: {},
+  lint: {
+    jsPlugins: [{ name: "vite-plus", specifier: "vite-plus/oxlint-plugin" }],
+    rules: { "vite-plus/prefer-vite-plus-imports": "error" },
+    options: { typeAware: true, typeCheck: true },
+  },
+  plugins: lazyPlugins(() => [solidStart(), nitro()]),
+});
