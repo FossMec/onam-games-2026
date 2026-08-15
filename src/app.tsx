e7edaecf531703743a85b8b53ca491e99371dcb7
dijith-481 <dijithdinesh@protonmail.com>
2026-08-15 16:26:54 +0530

    chore: add Google site verification

--- src/app.tsx
+++ src/app.tsx
@@ -21,6 +21,10 @@ export default function App() {
           />
           <Meta name="author" content="Dijith Dinesh" />
           <Meta name="creator" content="Dijith Dinesh" />
+          <Meta
+            name="google-site-verification"
+            content="-lINJGuul9m8kUB9WitBwq3UQZi8gWz52ncMnYWyc9I"
+          />
           <Meta property="og:title" content="FOSS ONAM — FOSS Onam Games by FOSS MEC" />
           <Meta
             property="og:description"
